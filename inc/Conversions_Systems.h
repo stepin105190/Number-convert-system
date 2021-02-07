3a51a376884e50997df2ae058439617697460b89
stepin105190 <69082292+stepin105190@users.noreply.github.com>
2021-02-07 13:57:13 +0530

    Add files via upload

---
+++ inc/Conversions_Systems.h
@@ -0,0 +1,7 @@
+#ifndef __CONVERSIONS_SYSTEMS_H__
+#define __CONVERSIONS_SYSTEMS_H__
+int Binary_to_Decimal(long long n);
+long Decimal_to_Binary(int n);
+int Octal_to_Decimal(int octal);
+int Decimal_to_Octal(int decimal);
+#endif
