604e9cf4195545890dc50f63a71a951ddcc6f738
stepin105190 <69082292+stepin105190@users.noreply.github.com>
2021-02-07 13:55:59 +0530

    Delete Conversions_Systems.h

--- Conversions_Systems.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CONVERSIONS_SYSTEMS_H__
-#define __CONVERSIONS_SYSTEMS_H__
-int Binary_to_Decimal(long long n);
-long Decimal_to_Binary(int n);
-int Octal_to_Decimal(int octal);
-int Decimal_to_Octal(int decimal);
-#endif
